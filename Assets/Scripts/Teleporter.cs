6864c723b06d0e54dfe918ec34fd115d57e5f18b
JJoker4 <JakubJan.Janas@gmail.com>
2017-12-16 12:08:16 +0100

    Enviroment

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+	public GameObject destination;
+
+	void OnTriggerEnter(Collider collider){
+
+		//if (collider.CompareTag("Player") || collider.gameObject.layer == 1) {
+		Vector3 targetPosition = destination.transform.position;
+		targetPosition += destination.transform.forward * 2; //new Vector3 (2, 3);
+		collider.transform.position = targetPosition;
+		}
+	}
+//}
